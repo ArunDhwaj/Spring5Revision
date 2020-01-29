198e703ac85ee2b25721501bb8def90325baf7a2
spb.arun.01@gmail.com <arun.dhwaj@smartbitpixel.co.in>
2020-01-29 15:11:16 +0530

    Initial set up

---
+++ src/main/java/com/sbp/Spring5Revision/with/Java8/and/above/Spring5RevisionWithJava8AndAboveApplication.java
@@ -0,0 +1,13 @@
+package com.sbp.Spring5Revision.with.Java8.and.above;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RevisionWithJava8AndAboveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RevisionWithJava8AndAboveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbp/Spring5Revision/with/Java8/and/above/Spring5RevisionWithJava8AndAboveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbp.Spring5Revision.with.Java8.and.above;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RevisionWithJava8AndAboveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
