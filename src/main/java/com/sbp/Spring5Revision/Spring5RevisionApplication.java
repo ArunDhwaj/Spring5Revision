4ab732a7e8b5ddbb39babd5b8fec1e462c276061
spb.arun.01@gmail.com <arun.dhwaj@smartbitpixel.co.in>
2020-01-29 16:08:47 +0530

    Refactor paths

---
+++ src/main/java/com/sbp/Spring5Revision/Spring5RevisionApplication.java
@@ -0,0 +1,13 @@
+package com.sbp.Spring5Revision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RevisionApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Spring5RevisionApplication.class, args);
+	}
+}
--- src/test/java/com/sbp/Spring5Revision/Spring5RevisionApplicationTests.java
+++ src/test/java/com/sbp/Spring5Revision/Spring5RevisionApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Spring5RevisionWithJava8AndAboveApplicationTests {
+class Spring5RevisionApplicationTests {
 
 	@Test
 	void contextLoads() {
